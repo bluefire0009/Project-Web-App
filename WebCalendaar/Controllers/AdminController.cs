2ea76e312f25b0e3742c9527f2b84709386ba75f
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-09-24 20:56:32 +0200

    Finish writing the AdminController

--- WebCalendaar/Controllers/AdminController.cs
+++ WebCalendaar/Controllers/AdminController.cs
@@ -1,29 +1,43 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebCalendaar.Models;
 [Route("api/Admin")]
 public class AdminController : Controller
 {
+    readonly IAdminStorage adminStorage;
+
+    public AdminController(IAdminStorage adminStorage)
+    {
+        this.adminStorage = adminStorage;
+    }
+
     [HttpPost("Add")]
     public async Task<IActionResult> AddAdmin([FromBody] Admin admin)
     {
-        return Ok();
+        await adminStorage.Create(admin);
+        return Ok($"Created Admin: {JsonConvert.SerializeObject(admin)}");
     }
 
-    [HttpPost("Get")]
+    [HttpGet("Get")]
     public async Task<IActionResult> GetAdmin([FromQuery] Guid id)
     {
-        return Ok();
+        Admin found = await adminStorage.Find(id);
+        return Ok(found);
     }
 
-    [HttpPost("Put")]
+    [HttpPut("Put")]
     public async Task<IActionResult> UpdateAdmin([FromBody] Admin admin,[FromQuery] Guid idToUpdate)
     {
-        return Ok();
+        await adminStorage.Delete(idToUpdate);
+        await adminStorage.Create(admin);
+        return Ok($"Updated Admin with Id={idToUpdate} to: {JsonConvert.SerializeObject(admin)}");
     }
 
-    [HttpPost("Delete")]
+    [HttpDelete("Delete")]
     public async Task<IActionResult> DeleteAdmin([FromQuery] Guid idToDelete)
     {
-        return Ok();
+        await adminStorage.Delete(idToDelete);
+        return Ok($"Deleted the Admin with Id={idToDelete}");
     }
 }
