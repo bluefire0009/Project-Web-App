7a5ec9f5b25823a70990f7094b4387d09f8fb380
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-09-30 22:56:47 +0200

    Add null checking to AdminController and AttendanceController

--- WebCalendaar/Controllers/AdminController.cs
+++ WebCalendaar/Controllers/AdminController.cs
@@ -14,13 +14,19 @@ public class AdminController : Controller
     [HttpPost("Add")]
     public async Task<IActionResult> AddAdmin([FromBody] Admin admin)
     {
+        if (admin == null){
+            return BadRequest("Null in the request");
+        }
         await adminStorage.Create(admin);
-        return Created($"Created Admin: \n",admin);
+        return Created($"Created Admin: ",admin);
     }
 
     [HttpGet("Get")]
     public async Task<IActionResult> GetAdmin([FromQuery] Guid id)
     {
+        if (adminStorage.Find(id).Result == null){
+            return NotFound($"Id : {id} not in the database");
+        }
         Admin found = await adminStorage.Find(id);
         return Ok(found);
     }
@@ -28,14 +34,22 @@ public class AdminController : Controller
     [HttpPut("Put")]
     public async Task<IActionResult> UpdateAdmin([FromBody] Admin admin,[FromQuery] Guid idToUpdate)
     {
+        if (admin == null){
+            return BadRequest("Null in the request");
+        } else if (adminStorage.Find(idToUpdate).Result == null){
+            return NotFound($"Id : {idToUpdate} not in the database");
+        }
         await adminStorage.Delete(idToUpdate);
         await adminStorage.Create(admin);
-        return Created($"Updated Admin with Id={idToUpdate} to: \n",admin);
+        return Created($"Updated Admin with Id={idToUpdate} to: ",admin);
     }
 
     [HttpDelete("Delete")]
     public async Task<IActionResult> DeleteAdmin([FromQuery] Guid idToDelete)
     {
+        if (adminStorage.Find(idToDelete).Result == null){
+            return NotFound($"Id : {idToDelete} not in the database");
+        }
         await adminStorage.Delete(idToDelete);
         return Ok($"Deleted the Admin with Id={idToDelete}");
     }
--- WebCalendaar/Controllers/AttendanceController.cs
+++ WebCalendaar/Controllers/AttendanceController.cs
@@ -14,13 +14,19 @@ public class AttendanceController : Controller
     [HttpPost("Add")]
     public async Task<IActionResult> AddAttendance([FromBody] Attendance attendance)
     {
+        if (attendance == null){
+            return BadRequest("Null in the request");
+        }
         await attendanceStorage.Create(attendance);
-        return Created($"Created Attendance: \n", attendance);
+        return Created($"Created Attendance: ", attendance);
     }
 
     [HttpGet("Get")]
     public async Task<IActionResult> GetAttendance([FromQuery] Guid id)
     {
+        if (attendanceStorage.Find(id).Result == null){
+            return NotFound($"Id : {id} not in the database");
+        }
         Attendance found = await attendanceStorage.Find(id);
         return Ok(found);
     }
@@ -28,14 +34,22 @@ public class AttendanceController : Controller
     [HttpPut("Put")]
     public async Task<IActionResult> UpdateAttendance([FromBody] Attendance attendance,[FromQuery] Guid idToUpdate)
     {
+        if (attendance == null){
+            return BadRequest("Null in the request");
+        } else if (attendanceStorage.Find(idToUpdate).Result == null){
+            return NotFound($"Id : {idToUpdate} not in the database");
+        }
         await attendanceStorage.Delete(idToUpdate);
         await attendanceStorage.Create(attendance);
-        return Created($"Updated Attendance with Id={idToUpdate} to: \n", attendance);
+        return Created($"Updated Attendance with Id={idToUpdate} to: ", attendance);
     }
 
     [HttpDelete("Delete")]
     public async Task<IActionResult> DeleteAttendance([FromQuery] Guid idToDelete)
     {
+        if (attendanceStorage.Find(idToDelete).Result == null){
+            return NotFound($"Id : {idToDelete} not in the database");
+        }
         await attendanceStorage.Delete(idToDelete);
         return Ok($"Deleted the Attendance with Id={idToDelete}");
     }
