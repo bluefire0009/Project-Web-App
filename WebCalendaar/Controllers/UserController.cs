5720b3e1ca0097c3002102a2089373820a6be767
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-09-30 22:57:29 +0200

    Change user id in controller and interface from int to Guid

--- WebCalendaar/Controllers/UserController.cs
+++ WebCalendaar/Controllers/UserController.cs
@@ -23,7 +23,7 @@ public class UserController : Controller
     }
 
     [HttpGet("Read")]
-    public async Task<IActionResult> Read([FromQuery] int userId)
+    public async Task<IActionResult> Read([FromQuery] Guid userId)
     {
         var user = await storage.Read(userId);
         if (user == null) return NotFound();
@@ -31,14 +31,14 @@ public class UserController : Controller
     }
 
     [HttpPut("Update")]
-    public async Task<IActionResult> Update([FromQuery] int userId, [FromBody] User user)
+    public async Task<IActionResult> Update([FromQuery] Guid userId, [FromBody] User user)
     {
         if (await storage.Update(userId, user)) return Ok("User updated");
         return NotFound();
     }
 
     [HttpDelete("Delete")]
-    public async Task<IActionResult> Delete([FromQuery] int userId)
+    public async Task<IActionResult> Delete([FromQuery] Guid userId)
     {
         if (await storage.Delete(userId)) return Ok("User deleted");
         return NotFound("user not found");
