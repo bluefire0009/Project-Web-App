646825a6f7d2fa28f3c6754baa3c778bd8505cef
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-10-02 20:40:49 +0200

    Add the boolean return type in admin storage interface

--- WebCalendaar/Interfaces/IAdminStorage.cs
+++ WebCalendaar/Interfaces/IAdminStorage.cs
@@ -1,8 +1,8 @@
 using WebCalendaar.Models;
 public interface IAdminStorage
 {
-    Task Create(Admin admin);
-    Task Delete(Guid adminId);
+    Task<bool> Create(Admin admin);
+    Task<bool> Delete(Guid adminId);
     Task<Admin?> Find(Guid adminId);
     Task<List<Admin>> FindMany(Guid[] adminIds);
 }
