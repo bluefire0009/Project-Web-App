260d62c83460f3fb33538c663c74ccb25434f521
bluefire0009 <1032628@hr.nl>
2024-10-02 20:09:08 +0200

    Move IAttendanceStorage into Interfaces folder

--- WebCalendaar/Services/IAdminStorage.cs
+++
@@ -1,9 +0,0 @@
-using WebCalendaar.Models;
-
-public interface IAdminStorage
-{
-    Task Create(Admin admin);
-    Task Delete(Guid adminId);
-    Task<Admin?> Find(Guid adminId);
-    Task<List<Admin>> FindMany(Guid[] adminIds);
-}
