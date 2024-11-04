bab31c9b71151b77b2d4bf57235fd722eb122a38
JohnSpruijt2 <johnspruijt@outlook.com>
2024-11-04 01:04:01 +0100

    updated CreateAttendance to properly use IEventStorage

--- WebCalendaar/Controllers/AttendAnEventController.cs
+++ WebCalendaar/Controllers/AttendAnEventController.cs
@@ -13,10 +13,10 @@ public class AttendAnEventController : Controller {
     }
 
     [HttpPost()]
-    public async Task<IActionResult> CreateAttendance([FromQuery] Guid eventId, [FromQuery] int userId, [FromQuery] string feedback = "") {
+    public async Task<IActionResult> CreateAttendance([FromQuery] int eventId, [FromQuery] int userId, [FromQuery] string feedback = "") {
         User? currentUser = await userStorage.Read(userId);
+        Event? pickedEvent = await eventStorage.Read(eventId);
 
-        Event? pickedEvent = await eventStorage.Find(_ => _.id == eventId);
         if (currentUser == null && pickedEvent == null) return NotFound("User and Event not found");
         else if (currentUser == null) return NotFound("User not found");
         else if (pickedEvent == null) return NotFound("Event not found");
