2b2f6b0779e992271b4b2adba4ec98b97a97234d
JohnSpruijt2 <johnspruijt@outlook.com>
2024-11-04 01:00:41 +0100

    added better error message

--- WebCalendaar/Controllers/AttendAnEventController.cs
+++ WebCalendaar/Controllers/AttendAnEventController.cs
@@ -13,12 +13,13 @@ public class AttendAnEventController : Controller {
     }
 
     [HttpPost()]
-    public async Task<IActionResult> CreateAttendance(Guid eventId, int userId, string feedback) {
+    public async Task<IActionResult> CreateAttendance([FromQuery] Guid eventId, [FromQuery] int userId, [FromQuery] string feedback = "") {
         User? currentUser = await userStorage.Read(userId);
-        if (currentUser == null) return NotFound("User not found");
 
         Event? pickedEvent = await eventStorage.Find(_ => _.id == eventId);
-        if (pickedEvent == null) return NotFound("Event not found");
+        if (currentUser == null && pickedEvent == null) return NotFound("User and Event not found");
+        else if (currentUser == null) return NotFound("User not found");
+        else if (pickedEvent == null) return NotFound("Event not found");
 
         Event_Attendance eventAttendance = new() {Feedback = feedback, User = currentUser, Event = pickedEvent};
         
