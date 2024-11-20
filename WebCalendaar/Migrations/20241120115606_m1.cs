6114d98265ae350419eb4e3ed7d4503d64a9ea3b
bluefire0009 <1032628@hr.nl>
2024-11-20 14:11:01 +0100

    Add rating and feedback to db

---
+++ WebCalendaar/Migrations/20241120115606_m1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCalendaar.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Feedback",
+                table: "Event_Attendance",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Event_Attendance",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Feedback",
+                table: "Event_Attendance");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Event_Attendance");
+        }
+    }
+}
