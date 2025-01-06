e0fc1aaea1ad89d44a6c72f500b603422109d0b7
bluefire0009 <1032628@hr.nl>
2025-01-06 14:05:31 +0100

    The header moved from SelectedDay

---
+++ FrontEnd/src/Components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header: React.FC = () => {
+    return <header className="navbar">
+        <a href="#">Homepage</a>
+        <a href="#">Events</a>
+        <a href="#">Calendar</a>
+        <a href="#">Log out</a>
+    </header>
+}
