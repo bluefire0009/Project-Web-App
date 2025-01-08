285ce34ec08e6279ab51c1e434fab89703bc4fd3
JohnSpruijt2 <johnspruijt@outlook.com>
2025-01-08 13:51:16 +0100

    Google controller now is able to push something into the primary google calendar if you provide api key and OAuthkey

--- WebCalendaar/Controllers/GoogleController.cs
+++ WebCalendaar/Controllers/GoogleController.cs
@@ -1,10 +1,7 @@
-using Microsoft.AspNetCore.Authorization; // For auth attributes
 using Microsoft.AspNetCore.Mvc;
 using WebCalendaar.Models;
-using System;
-using System.Threading.Tasks;
-using System.Linq;
-using Microsoft.Identity.Client.Extensions.Msal;
+using System.Text;
+using System.Text.Json;
 
 [Route("api/google")]
 public class GoogleController : Controller
@@ -12,6 +9,19 @@ public class GoogleController : Controller
     [HttpGet("syncAll")]
     public async Task<IActionResult> SyncAll()
     {
-        return Ok();
+        HttpClient client = new HttpClient();
+        // api key header
+        client.DefaultRequestHeaders.Add("key", "");
+
+        // OAuth key header
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "");
+
+        // event Json
+        string jsonData = "{\"summary\":\"SampleEvent\",\"location\":\"123ExampleSt,ExampleCity,EX\",\"description\":\"Adetaileddescriptionoftheevent\",\"start\":{\"dateTime\":\"2025-01-09T09:00:00-07:00\",\"timeZone\":\"America/Los_Angeles\"},\"end\":{\"dateTime\":\"2025-01-09T10:00:00-07:00\",\"timeZone\":\"America/Los_Angeles\"},\"recurrence\":[{\"freq\":\"daily\",\"interval\":1,\"count\":5}],\"attendees\":[{\"email\":\"attendee1@example.com\"},{\"email\":\"attendee2@example.com\"}],\"reminders\":{\"useDefault\":false,\"overrides\":[{\"method\":\"popup\",\"minutes\":10},{\"method\":\"email\",\"minutes\":1440}]}}";
+
+        // send request
+        HttpContent postContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var result = client.PostAsync("https://www.googleapis.com/calendar/v3/calendars/primary/events", postContent).GetAwaiter().GetResult();
+        return Ok(result);
     }
 }
