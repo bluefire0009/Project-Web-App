f64ad798551930be7eed663ed75c3437e958d04f
JohnSpruijt2 <johnspruijt@outlook.com>
2025-01-10 21:03:20 +0100

    Completed the backend for the google calendar syncing all events

--- WebCalendaar/Controllers/GoogleController.cs
+++ WebCalendaar/Controllers/GoogleController.cs
@@ -6,22 +6,78 @@ using System.Text.Json;
 [Route("api/google")]
 public class GoogleController : Controller
 {
+    readonly IEventStorage _eventStorage;
+    
+    private readonly IEventAttendanceStorage _eventAttendanceStorage;
+
+    public GoogleController(IEventStorage eventStorage, IEventAttendanceStorage eventAttendanceStorage)
+    {
+        _eventStorage = eventStorage;
+        _eventAttendanceStorage = eventAttendanceStorage;
+    }
+
     [HttpGet("syncAll")]
     public async Task<IActionResult> SyncAll()
     {
         HttpClient client = new HttpClient();
+
+        List<Event_Attendance> event_Attendances = await this._eventAttendanceStorage.GetAllByUser(4);
+        List<Event> allEvents = await this._eventStorage.GetAllUpcomingByIds(event_Attendances.Select(_ => _.EventId).ToList());
+
+        var googleCalendarEvents = allEvents.Select(e => new
+        {
+            summary = e.Title,
+            location = e.Location,
+            description = e.Description,
+            start = new
+            {
+                dateTime = $"{e.EventDate.ToString("yyyy-MM-dd")}T{e.StartTime}+00:00",
+            },
+            end = new
+            {
+                dateTime = $"{e.EventDate.ToString("yyyy-MM-dd")}T{e.EndTime}+00:00",
+            },
+            // recurrence = new[]
+            // {
+            //     new { freq = "daily", interval = 1, count = 5 }
+            // },
+            // attendees = new[]
+            // {
+            //     new { email = "attendee1@example.com" },
+            //     new { email = "attendee2@example.com" }
+            // },
+            reminders = new
+            {
+                useDefault = false,
+                // new[]
+                // {
+                //     new { method = "popup", minutes = 10 },
+                //     new { method = "email", minutes = 1440 }
+                // }
+            }
+        }).ToList();
+        
+        foreach (var googleCalendarEvent in googleCalendarEvents)
+        {
+            if (!await SendToGoogleCalendar(JsonSerializer.Serialize(googleCalendarEvent))) {
+                return BadRequest("something went wrong");
+            }
+        }
+
+        return Ok("success");
+    }
+
+    private async Task<bool> SendToGoogleCalendar(string jsonData) {
+        HttpClient client = new HttpClient();
         // api key header
-        client.DefaultRequestHeaders.Add("key", "");
+        client.DefaultRequestHeaders.Add("key", "insert api key here");
 
         // OAuth key header
-        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "");
-
-        // event Json
-        string jsonData = "{\"summary\":\"SampleEvent\",\"location\":\"123ExampleSt,ExampleCity,EX\",\"description\":\"Adetaileddescriptionoftheevent\",\"start\":{\"dateTime\":\"2025-01-09T09:00:00-07:00\",\"timeZone\":\"America/Los_Angeles\"},\"end\":{\"dateTime\":\"2025-01-09T10:00:00-07:00\",\"timeZone\":\"America/Los_Angeles\"},\"recurrence\":[{\"freq\":\"daily\",\"interval\":1,\"count\":5}],\"attendees\":[{\"email\":\"attendee1@example.com\"},{\"email\":\"attendee2@example.com\"}],\"reminders\":{\"useDefault\":false,\"overrides\":[{\"method\":\"popup\",\"minutes\":10},{\"method\":\"email\",\"minutes\":1440}]}}";
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "insert auth key here");
 
         // send request
         HttpContent postContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        var result = client.PostAsync("https://www.googleapis.com/calendar/v3/calendars/primary/events", postContent).GetAwaiter().GetResult();
-        return Ok(result);
+        var result = await client.PostAsync("https://www.googleapis.com/calendar/v3/calendars/primary/events", postContent);
+        return result.StatusCode == System.Net.HttpStatusCode.OK;
     }
 }
