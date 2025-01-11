06967a3fd7f8b3c041bd436334a0a3dff9bc08c2
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2025-01-11 21:25:55 +0100

    Split fetchAllEvents into multiple lines and ensure that each fetched object is parsed into a CalendarEvent object

--- FrontEnd/src/DataLoader.ts
+++ FrontEnd/src/DataLoader.ts
@@ -1,6 +1,18 @@
-import { CalendarEvent } from "./States/CalendarState";
+import { CalendarEvent, CalendarEventConstructor } from "./States/CalendarState";
 import Api_url from "./Components/Api_url"
 
-export const fetchAllEvents:()=> Promise<CalendarEvent[]> = async () => await fetch(`${Api_url}/api/events/get/all`)
+export const fetchAllEvents:()=> Promise<CalendarEvent[]> = async () => {
+        const events = await fetch(`${Api_url}/api/events/get/all`)
         .then(response => response.json())
+        const parsedEvents: CalendarEvent[] = events.map((event: any) => 
+                CalendarEventConstructor(
+                        BigInt(event.eventId),
+                        event.title,
+                        event.description,
+                        new Date(`${event.eventDate}T${event.startTime}`),
+                        new Date(`${event.eventDate}T${event.endTime}`),
+                        event.location
+                ));
+        return parsedEvents;
+}
         
