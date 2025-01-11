c0f5aae634d3bef05f628fb0f2b405daaaba2be4
bluefire0009 <1032628@hr.nl>
2025-01-11 20:29:09 +0100

    Make the CalendarPage fetch events and pass them to calendar components

--- FrontEnd/src/Components/CalendarPage.tsx
+++ FrontEnd/src/Components/CalendarPage.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Footer } from "./Footer"
 import { WeekCalendar } from "./WeekCalendar"
 import { MonthCalendar } from "./MonthCalendar"
 import { HourDisplay } from "./HourDisplay"
+import { CalendarEvent } from "../States/CalendarState"
+import { fetchAllEvents } from "../DataLoader"
 
 export const CalendarPage: React.FC = () =>{
     const [selectedCalendar, setSelectedCalendar] = useState<CurrentlySelectedCalendar>(CurrentlySelectedCalendar.WeeklyCalendar)
-    
+    const [events, setCurrentEvents] = useState<CalendarEvent[]>([]);
+
+    useEffect(() => {
+        const loadEvents = async () => {
+            try {
+                const fetchedEvents = await fetchAllEvents();
+                console.log("------")
+                setCurrentEvents(fetchedEvents); // Update the state with fetched events
+                
+            } catch (error) {
+                setCurrentEvents([]);
+                console.log(error)
+            }
+        };
+        loadEvents();
+        console.log(events);
+    }, []);
+
     const ChangeCalendarButtons: React.FC = () =>{
         return <span className="CalendarButtons">
             <button className="WeeklyCalendarButton"
@@ -23,7 +42,7 @@ export const CalendarPage: React.FC = () =>{
     return <div className="CalendarPage">
         <ChangeCalendarButtons/>
         <div className="CalendarSection">
-            {selectedCalendar == CurrentlySelectedCalendar.WeeklyCalendar? <span style={{display:"flex"}}><HourDisplay/><WeekCalendar/></span>:<MonthCalendar/>}
+            {selectedCalendar == CurrentlySelectedCalendar.WeeklyCalendar? <span style={{display:"flex"}}><HourDisplay/><WeekCalendar events={events}/></span>:<MonthCalendar events={events}/>}
         </div>
     </div>
 }
