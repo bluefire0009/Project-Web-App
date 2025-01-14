23098526634aff2d95dff3e0cdfb506391c127dc
MilanVHR <1034676@hr.nl>
2025-01-14 11:18:04 +0100

    Add functionality to post a review on frontend side

--- FrontEnd/src/Components/Reviews.tsx
+++ FrontEnd/src/Components/Reviews.tsx
@@ -69,6 +69,7 @@ const Reviews: React.FC = () => {
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
+          credentials: "include"
         }
       );
   
